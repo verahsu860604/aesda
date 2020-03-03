68dbdcd779644a68ed9749fe8488cb2a8a2e9abf
Wei-Hsuan Hsu <weihsuan_hsu@berkeley.edu>
2020-03-03 12:20:03 -0800

    add market & ess default values, fixes #1 #2

--- js/ess.js
+++ js/ess.js
@@ -5,6 +5,102 @@ const url = require("url");
 const remote = electron.remote;
 const ipc = electron.ipcRenderer;
 
+const defaultVal = {
+    'Power Flow Battery': {
+        'ei-numOfEss': 1,
+        'ei-selfDischareRatio': 1,
+        'ei-dimen': 1,
+        'ei-cost': 1,
+        'ei-inEffi': 1,
+        'ei-outEffi': 1,
+        'ei-maxpin': 1,
+        'ei-maxpout': 1,
+        'ei-minsoc': 1,
+        'ei-maxsoc': 1,
+        'ei-p1c': 1,
+        'ei-p1d': 1,
+        'ei-p2c': 1,
+        'ei-p2d': 1,
+        'ei-p3c': 1,
+        'ei-p3d': 1,
+        'ei-p4c': 1,
+        'ei-p4d': 1,
+        'ei-p5c': 1,
+        'ei-p5d': 1,
+        'ei-p6c': 1,
+        'ei-p6d': 1,
+    }, 'Lithium-Ion': {
+        'ei-numOfEss': 2,
+        'ei-selfDischareRatio': 1,
+        'ei-dimen': 1,
+        'ei-cost': 1,
+        'ei-inEffi': 1,
+        'ei-outEffi': 1,
+        'ei-maxpin': 1,
+        'ei-maxpout': 1,
+        'ei-minsoc': 1,
+        'ei-maxsoc': 1,
+        'ei-p1c': 1,
+        'ei-p1d': 1,
+        'ei-p2c': 1,
+        'ei-p2d': 1,
+        'ei-p3c': 1,
+        'ei-p3d': 1,
+        'ei-p4c': 1,
+        'ei-p4d': 1,
+        'ei-p5c': 1,
+        'ei-p5d': 1,
+        'ei-p6c': 1,
+        'ei-p6d': 1,
+    }, 'Supercapacitor': {
+        'ei-numOfEss': 3,
+        'ei-selfDischareRatio': 1,
+        'ei-dimen': 1,
+        'ei-cost': 1,
+        'ei-inEffi': 1,
+        'ei-outEffi': 1,
+        'ei-maxpin': 1,
+        'ei-maxpout': 1,
+        'ei-minsoc': 1,
+        'ei-maxsoc': 1,
+        'ei-p1c': 1,
+        'ei-p1d': 1,
+        'ei-p2c': 1,
+        'ei-p2d': 1,
+        'ei-p3c': 1,
+        'ei-p3d': 1,
+        'ei-p4c': 1,
+        'ei-p4d': 1,
+        'ei-p5c': 1,
+        'ei-p5d': 1,
+        'ei-p6c': 1,
+        'ei-p6d': 1,
+    }, 'Custom': {
+        'ei-numOfEss': 4,
+        'ei-selfDischareRatio': 1,
+        'ei-dimen': 1,
+        'ei-cost': 1,
+        'ei-inEffi': 1,
+        'ei-outEffi': 1,
+        'ei-maxpin': 1,
+        'ei-maxpout': 1,
+        'ei-minsoc': 1,
+        'ei-maxsoc': 1,
+        'ei-p1c': 1,
+        'ei-p1d': 1,
+        'ei-p2c': 1,
+        'ei-p2d': 1,
+        'ei-p3c': 1,
+        'ei-p3d': 1,
+        'ei-p4c': 1,
+        'ei-p4d': 1,
+        'ei-p5c': 1,
+        'ei-p5d': 1,
+        'ei-p6c': 1,
+        'ei-p6d': 1,
+    }
+}
+
 ipc.on('essType', (event, args) => {
     essType = args[0];
     essId = args[1]
@@ -16,6 +112,7 @@ ipc.on('essType', (event, args) => {
         });
     }else {
         // todo: add default value
+        setDefault(defaultVal[essType])
     }
 })
 
@@ -27,3 +124,10 @@ document.getElementById('submitBtn').addEventListener('click', (event) => {
 document.getElementById('cancelBtn').addEventListener('click', (event) => {
     remote.getCurrentWindow().close();
 })
+
+function setDefault(val) {
+    var keys = Object.keys(val)
+    keys.forEach(e => {
+        document.getElementsByName(e)[0].defaultValue = val[e]
+    })
+}
