e61dd82614e4832d25c84cf187542ac4f51ea116
Kingsley Yang <cj.yang@berkeley.edu>
2020-03-16 13:34:49 -0700

    add mapping func

--- algo/param_mapping.py
+++ algo/param_mapping.py
@@ -1,10 +1,12 @@
-configMapping = 
+import sys
+import ast
+config_mapping = \
 {
-    "ci-predic" : "planning_horizon",
+    'ci-predic' : 'planning_horizon',
     
     # TODO should be moved to market
-    # "ci-maxpIn"
-    # "ci-maxpOut"
+    # 'ci-maxpIn'
+    # 'ci-maxpOut'
     
     # TODO not found in UI
     # : 'soh_update_interval'
@@ -12,29 +14,29 @@ configMapping =
     
 }
 
-essMapping = 
+ess_mapping = \
 {
     # TODO shouldn't matter
-    # "ei-numOfEss"           :
+    # 'ei-numOfEss'           :
     #                         : energy_type
 
 
-    "ei-selfDischareRatio"  :   "self_discharge_ratio",
+    'ei-selfDischareRatio'  :   'self_discharge_ratio',
     # TODO not found in python
-    # "ei-dimen"              :   
-    "ei-cost"               :   "cost",
+    # 'ei-dimen'              :   
+    'ei-cost'               :   'cost',
     # TODO not found in python
-    # "ei-othercost"          :
-    "ei-inEffi"             :   "efficiency_upward",
-    "ei-outEffi"            :   "efficiency_downward",
+    # 'ei-othercost'          :
+    'ei-inEffi'             :   'efficiency_upward',
+    'ei-outEffi'            :   'efficiency_downward',
     # TODO mismatch
-    # "ei-threshold"          :   
+    # 'ei-threshold'          :   
     # : soc_profile_max_input_th
     # : soc_profile_min_output_th
-    "ei-maxpin"             :   "soc_profile_max_power_upward",
-    "ei-maxpout"            :   "soc_profile_max_power_downward",
-    "ei-minsoc"             :   "soc_profile_min_soc",
-    "ei-maxsoc"             :   "soc_profile_max_soc",
+    'ei-maxpin'             :   'soc_profile_max_power_upward',
+    'ei-maxpout'            :   'soc_profile_max_power_downward',
+    'ei-minsoc'             :   'soc_profile_min_soc',
+    'ei-maxsoc'             :   'soc_profile_max_soc',
 
     # TODO not found in UI
     # : soc_profile_energy_scale
@@ -50,26 +52,28 @@ essMapping =
     # : visualize
     
     # dod, cycles
-    "ei-p1d" : "d1",
-    "ei-p1c" : "c1",
-    "ei-p2d" : "d2",
-    "ei-p2c" : "c2",
-    "ei-p3d" : "d3",
-    "ei-p3c" : "c3",
-    "ei-p4d" : "d4",
-    "ei-p4c" : "c4",
-    "ei-p5d" : "d5",
-    "ei-p5c" : "c5",
-    "ei-p6d" : "d6",
-    "ei-p6c" : "c6"
+    'ei-p1d' : 'd1',
+    'ei-p1c' : 'c1',
+    'ei-p2d' : 'd2',
+    'ei-p2c' : 'c2',
+    'ei-p3d' : 'd3',
+    'ei-p3c' : 'c3',
+    'ei-p4d' : 'd4',
+    'ei-p4c' : 'c4',
+    'ei-p5d' : 'd5',
+    'ei-p5c' : 'c5',
+    'ei-p6d' : 'd6',
+    'ei-p6c' : 'c6'
 }
 
-marketMapping = 
+market_mapping = \
 {
     # TODO interval mismatch
-    # "mi-upItv"      :    
-    # "mi-dwnItv"     :   
-    # "mi-minItv"     :   
+    # 'mi-upItv'      :    
+    # 'mi-dwnItv'     :   
+    # 'mi-minItv'     :   
+    # : max_feasible_power_percentage
+    # : min_feasible_power_percentage
     # : price_cyclic_n_upward
     # : price_cyclic_n_downward
     # : percentage_cyclic_n
@@ -78,22 +82,73 @@ marketMapping =
     # : percentage_cyclic_eps
     # : percentage_fixed
 
-    "mi-spUpdate"   :   "setpoint_interval",
-    "mi-twDelivery" :   "time_window_in_delivery",
-    "mi-upMinPrice" :   "min_feasible_buying_price",
-    "mi-upMaxPrice" :   "max_feasible_buying_price",
-    "mi-dwnMinPrice":   "min_feasible_selling_price",
-    "mi-dwnMaxPrice":   "max_feasible_selling_price",
-    "mi-planning"   :   "planning_phase_length",
-    "mi-schedule"   :   "selection_phase_length",
-    "mi-selection"  :   "schedule_phase_length ",
-    "mi-delivery"   :   "delivery_phase_length"
+    'mi-spUpdate'   :   'setpoint_interval',
+    'mi-twDelivery' :   'time_window_in_delivery',
+    'mi-upMinPrice' :   'min_feasible_buying_price',
+    'mi-upMaxPrice' :   'max_feasible_buying_price',
+    'mi-dwnMinPrice':   'min_feasible_selling_price',
+    'mi-dwnMaxPrice':   'max_feasible_selling_price',
+    'mi-planning'   :   'planning_phase_length',
+    'mi-schedule'   :   'selection_phase_length',
+    'mi-selection'  :   'schedule_phase_length ',
+    'mi-delivery'   :   'delivery_phase_length'
     
     # TODO no penalty in python
-    # "mi-upPenalty"  :   "" 
-    # "mi-dwnPenalty" :   ""
+    # 'mi-upPenalty'  :   '' 
+    # 'mi-dwnPenalty' :   ''
 
     # TODO not found in UI
     # : data_loader
     # : test_mode
 }
+
+
+def map_param(param_ui):
+    print(config_mapping)
+    print(market_mapping)
+    print(ess_mapping)
+    sys.stdout.flush()
+    param = {}
+    for k, v in param_ui.items():
+        if k == 'configForm': # v is a list
+            param['config'] = {}
+            for config_dict in v:
+                key = config_mapping.get(config_dict['name'])
+                if key: # if key exists in config_mapping
+                    val = config_dict['value']
+                    if val:
+                        val = ast.literal_eval(val)
+                    else:
+                        val = 0
+                    param['config'][key] = val
+
+        elif k == 'marketObjList': # v is a dict
+            param['markets'] = []
+            for market_ID, market_list in v.items():
+                param['markets'].append({})
+                for market_dict in market_list:
+                    key = market_mapping.get(market_dict['name'])
+                    if key: # if key exists in market_mapping
+                        val = market_dict['value']
+                        if val:
+                            val = ast.literal_eval(val)
+                        else:
+                            val = 0
+                        param['markets'][-1][key] = val
+
+        elif k == 'essObjList':
+            param['energy_sources'] = []
+            for battery_type, battery_type_dict in v.items():
+                for type_num, type_list in battery_type_dict.items():
+                    param['energy_sources'].append({})
+                    for battery_dict in type_list:
+                        key = ess_mapping.get(battery_dict['name']) 
+                        if key: # if key exists in ess_mapping
+                            val = battery_dict['value']
+                            if val:
+                                val = ast.literal_eval(val)
+                            else:
+                                val = 0
+                            param['energy_sources'][-1][key] = val
+
+    return param
