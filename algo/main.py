d748ad8e38ebd0d007b105ea9166bf0d890f8bbe
KeiraZhaoYX <keira_zhao@berkeley.edu>
2020-03-15 19:45:33 -0700

    Fit the tunning parameters with simulated data and get battery life from soh

--- algo/main.py
+++ algo/main.py
@@ -12,6 +12,8 @@ if True:
 
     config = config.Config(**data['config'])
     energy_sources = [energy_source.EnergySource(**kwargs) for kwargs in data['energy_sources']]
+    for energy_source in energy_sources:
+        energy_source.tunning_parameter()
     markets = [market.Market(**kwargs) for kwargs in data['markets']]
     algo2 = mpc_solver.MPCSolver(config=config, markets=markets, energy_sources=energy_sources)
 
