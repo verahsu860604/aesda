18634d48b3bd18647e5e5a3ac00a30ee2caadcbb
Wei-Hsuan Hsu <weihsuan_hsu@berkeley.edu>
2020-03-16 01:28:04 -0700

    del redundant lines

--- main.js
+++ main.js
@@ -120,8 +120,6 @@ ipc.on('createMarketWindow', (event, args) => {
 })
 
 ipc.on('createEssWindow', (event, args) => {
-    var essType = args[0]
-    var essObjNum = args[1]
     createEssWindow([essType, essObjNum[essType]+1, ''])
     
 })
