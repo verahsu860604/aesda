4283774998bdf023b86c3762594e271546848f59
KeiraZhaoYX <keira_zhao@berkeley.edu>
2020-04-17 22:44:46 -0700

    speed up visual

--- algo/visual.py
+++ algo/visual.py
@@ -1,12 +1,15 @@
 import energy_source
 import sys
 assert len(sys.argv) > 1
-visual = energy_source.Visualization(dod_profile_change_th=float(sys.argv[1]),dimension = int(sys.argv[2]))
+visual = energy_source.Visualization(dod_profile_change_th=float(sys.argv[1]))
 real_time, real_cycle = visual.threshold_test()
 for i in range(len(real_time)):
 	print([real_time[i],real_cycle[i]])
+# print("[")
+# for i in range(len(real_time)):
+# 	print("{x:%d,y:%f},"%(real_time[i],real_cycle[i]), end = "")
 
-
+# print("]")
 
 
 
