fe7149f3da020d2ec01ab7063ed5ce5decb22b5e
chenkaiyu1997 <justryit8@gmail.com>
2020-05-02 22:55:39 -0700

    add drawing

--- algo/draw1.py
+++ algo/draw1.py
@@ -11,110 +11,113 @@ import cyclic_coordinate
 import pareto
 
 parameters = {
+    'markets': 
+    [
+    {
+        'downward_penalty': 400,
+        'setpoint_interval': 1,
+        'name': 'Secondary Reserve',
+        'schedule_phase_length': 60,
+        'price_data_path': '/Users/justryit/Desktop/aesda/algo/data/secondary_price.csv',
+        'max_feasible_power_percentage': 80,
+        'price_cyclic_eps_upward': 25,
+        'min_feasible_selling_price': 100,
+        'upward_penalty': 30,
+        'price_cyclic_eps_downward': 300,
+        'planning_phase_length': 0,
+        'min_feasible_buying_price': 5,
+        'time_window_in_delivery': 4,
+        'percentage_cyclic_n': 1,
+        'max_feasible_buying_price': 30,
+        'delivery_phase_length': 240,
+        'max_feasible_selling_price': 400,
+        'price_cyclic_n_downward': 5,
+        'setpoint_data_path': '/Users/justryit/Desktop/aesda/algo/data/secondary_setpoint.csv',
+        'min_feasible_power_percentage': 20,
+        'percentage_fixed': 1,
+        'price_cyclic_n_upward': 4,
+        'percentage_cyclic_eps': 60,
+        'selection_phase_length': 0
+    },
+    {
+        'downward_penalty': 400,
+        'setpoint_interval': 15,
+        'name': 'Tertiary Reserve',
+        'schedule_phase_length': 60,
+        'price_data_path': '/Users/justryit/Desktop/aesda/algo/data/tertiary_price.csv',
+        'max_feasible_power_percentage': 80,
+        'price_cyclic_eps_upward': 25,
+        'min_feasible_selling_price': 100,
+        'upward_penalty': 30,
+        'price_cyclic_eps_downward': 300,
+        'planning_phase_length': 0,
+        'min_feasible_buying_price': 5,
+        'time_window_in_delivery': 4,
+        'percentage_cyclic_n': 1,
+        'max_feasible_buying_price': 30,
+        'delivery_phase_length': 240,
+        'max_feasible_selling_price': 400,
+        'price_cyclic_n_downward': 5,
+        'setpoint_data_path': '/Users/justryit/Desktop/aesda/algo/data/tertiary_setpoint.csv',
+        'min_feasible_power_percentage': 20,
+        'percentage_fixed': 0,
+        'price_cyclic_n_upward': 4,
+        'percentage_cyclic_eps': 60,
+        'selection_phase_length': 0
+    }],
     'energy_sources': [
-        {
-            'energy_type': 'Lithium-Ion',
-            'soc_profile_energy_scale': 5,
-            'soc_profile_max_input_th': 90,
-            'soc_profile_min_output_th': 10,
-            'soc_profile_max_power_upward': 10,
-            'soc_profile_max_power_downward': 10,
-            'efficiency_upward': 0.8,
-            'efficiency_downward': 0.8,
-            'cost': 310,
-            'dod_profile': True,
-            'd1': 2,
-            'c1': 10000000,
-            'd2': 4,
-            'c2': 1000000,
-            'd3': 17,
-            'c3': 100000,
-            'd4': 30,
-            'c4': 40000,
-            'd5': 60,
-            'c5': 10000,
-            'd6': 100,
-            'c6': 3000
-        },
-        # {
-        #     'energy_type': 'PowerFlow',
-        #     'soc_profile_energy_scale': 20,
-        #     'soc_profile_max_input_th': 70,
-        #     'soc_profile_min_output_th': 30,
-        #     'soc_profile_max_power_upward': 10,
-        #     'soc_profile_max_power_downward': 10,
-        #     'efficiency_upward': 0.9,
-        #     'efficiency_downward': 0.9,
-        #     'cost': 470,
-        #     'dod_profile': False,
-        #     'd1': 0,
-        #     'c1': 10,
-        #     'd2': 0,
-        #     'c2': 10,
-        #     'd3': 0,
-        #     'c3': 0,
-        #     'd4': 0,
-        #     'c4': 0,
-        #     'd5': 0,
-        #     'c5': 0,
-        #     'd6': 0,
-        #     'c6': 0
-        # }
-    ],
-    'markets': [
-        {
-            "time_window_in_delivery": 4, # Primary
-            "planning_phase_length": 60,
-            "selection_phase_length": 60,
-            "schedule_phase_length": 60,
-            "delivery_phase_length": 60,
-            "price_cyclic_n_upward": 2,
-            "price_cyclic_n_downward": 2,
-            "price_cyclic_eps_downward": 80,
-            "price_cyclic_eps_upward": 16,
-            "max_feasible_selling_price": 250,
-            "min_feasible_selling_price": 150,
-            "min_feasible_buying_price": 1,
-            "max_feasible_buying_price": 20,
-            "setpoint_interval": 1,
-            # "percentage_fixed": True,
-            'price_data_path': 'data/primary_price.csv',
-            'setpoint_data_path': 'data/primary_setpoint.csv'
-        },
-        # {
-        #     "time_window_in_delivery": 4, # Secondary
-        #     "planning_phase_length": 120,
-        #     "selection_phase_length": 120,
-        #     "schedule_phase_length": 120,
-        #     "delivery_phase_length": 120,
-        #     "setpoint_interval": 15,
-        #     "price_cyclic_n_upward": 2,
-        #     "price_cyclic_n_downward": 2,
-        #     "price_cyclic_eps_downward": 80,
-        #     "price_cyclic_eps_upward": 16,
-        #     "max_feasible_selling_price": 250,
-        #     "min_feasible_selling_price": 150,
-        #     "min_feasible_buying_price": 1,
-        #     "max_feasible_buying_price": 20,
-        #     # "percentage_fixed": True,
-        #     'price_data_path': 'data/primary_price.csv',
-        #     'setpoint_data_path': 'data/primary_setpoint.csv'
-        # },
-        # {
-        #     "time_window_in_delivery": 4, # Tertiary
-        #     "planning_phase_length": 960,
-        #     "selection_phase_length": 960,
-        #     "schedule_phase_length": 960,
-        #     "delivery_phase_length": 960,
-        #     "setpoint_interval": 60,
-        #     # 'price_data_path': 'data/primary_price.csv',
-        #     # 'setpoint_data_path': 'data/primary_setpoint.csv'
-        # },
-    ],
-    'config':{
-        'planning_horizon': 360,
-        'soh_update_interval': 1440,
-        'tot_timestamps': 10080
+    {
+        'other_cost': 0,
+        'name': 'Lithium-Ion',
+        'c2': 1000000,
+        'soc_profile_max_soc': 80,
+        'd4': 30,
+        'soc_profile_min_soc': 20,
+        'c6': 3000,
+        'd2': 4,
+        'c5': 10000,
+        'd1': 2,
+        'cost': 310,
+        'efficiency_upward': 0.95,
+        'soc_profile_energy_scale': 20,
+        'c3': 100000,
+        'soc_profile_min_output_th': 30,
+        'c1': 10000000,
+        'soc_profile_max_input_th': 70,
+        'soc_profile_max_power_downward': 10,
+        'c4': 40000,
+        'efficiency_downward': 0.95,
+        'd6': 100,
+        'soc_profile_max_power_upward': 10,
+        'dod_profile_change_th': 0.2,
+        'd5': 60,
+        'self_discharge_ratio': 0,
+        'd3': 17
+    },
+    {
+        'other_cost': 0,
+        'soc_profile_max_power_downward': 10,
+        'soc_profile_max_soc': 100,
+        'name': 'Power Flow Battery',
+        'efficiency_downward': 0.78,
+        'soc_profile_max_power_upward': 10,
+        'dod_profile_change_th': 0.2,
+        'soc_profile_min_soc': 0,
+        'soc_profile_max_input_th': 70,
+        'cost': 470,
+        'self_discharge_ratio': 0,
+        'efficiency_upward': 0.78,
+        'soc_profile_energy_scale': 40,
+        'soc_profile_min_output_th': 30
+    }
+ ],
+ 'config': 
+    {
+        'tot_timestamps': 43200,
+        'planning_horizon': 60,
+        'soh_update_interval': 4320,
+        'strategy': 0,
+        'optimizer': 0
     }
 }
 
@@ -125,7 +128,7 @@ def get_parameters():
 
 import time
 
-for horizon in range(100, 3000, 100):
+for horizon in range(1, 300, 10):
     start = time.time()
 
     data = get_parameters()
@@ -135,7 +138,7 @@ for horizon in range(100, 3000, 100):
     markets = [market.Market(**kwargs) for kwargs in data['markets']]
     mpc = mpc_solver.MPCSolver(config=myconfig, markets=markets, energy_sources=energy_sources)
 
-    results = mpc.solve([[10, 200] for i in range(len(markets))], ['free' for i in range(len(markets))])
+    results = mpc.solve([[30, 150] for i in range(len(markets))], ['free' for i in range(len(markets))])
     revenue = 0
     penalty = 0
     for time_k in range(len(results)):
